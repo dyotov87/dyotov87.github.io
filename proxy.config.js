5200b3f66090a6f9e0afd8529360f34c7ae554a1
Dimitar.Yotov <dimitar.yotov@blu11.com>
2021-06-25 13:49:02 +0300

    2

--- proxy.config.js
+++ proxy.config.js
@@ -2,20 +2,20 @@ const PROXY_CONFIG = [
   {
     context: ['/api/**', '/api-web/**', '/predict-api/**', '/tenant/**', '/login**', '/oauth/**', '/auth/**', '/logout**'],
     //target: 'http://127.0.0.1:4300/',
-    target: 'https://auth.eu.yuuvis.io/auth/',
+    target: 'https://eu.yuuvis.io/',
     changeOrigin: true,
     secure: false,
     logLevel: 'debug'
   },
-  // {
-  //   context: ['/viewer/**'],
-  //   // target: 'http://127.0.0.1:9000',
-  //   //target: 'http://127.0.0.1:4300/',
-  //   target: 'https://auth.eu.yuuvis.io/auth/',
-  //   changeOrigin: true,
-  //   secure: false,
-  //   logLevel: 'debug'
-  // }
+  {
+    context: ['/viewer/**'],
+    // target: 'http://127.0.0.1:9000',
+    //target: 'http://127.0.0.1:4300/',
+    target: 'https://eu.yuuvis.io/',
+    changeOrigin: true,
+    secure: false,
+    logLevel: 'debug'
+  }
 ];
 
 module.exports = PROXY_CONFIG;
